5733c092d1fd61a5517fd3591195127f2f617b3b
Lucas <barbosals298@gmail.com>
2024-10-10 06:48:13 -0300

    Adicionando mensagem e corrigindo codigo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,13 +3,15 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Por favor, digite seu nome: ");
+        System.out.print("Por favor, digite seu nome: ");
         String nomeCliente = sc.next();
-        System.out.println("Por favor, digite o número da Agencia!: ");
-        String numeroAgencia = sc.next();
-        System.out.println("Por favor, digite o número da conta: ");
+        System.out.print("Por favor, digite o número da conta: ");
         int numeroConta = sc.nextInt();
-        System.out.println("por favor, deposite um novo saldo: ");
-        Double saldo = sc.nextDouble();
+        System.out.print("Por favor, digite o número da Agencia!: ");
+        String numeroAgencia = sc.next();
+        System.out.print("por favor, deposite um novo saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
